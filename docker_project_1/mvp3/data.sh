e1d83907620bbd084948513e07c53c95269a5277
Miguel Apestegui <mhapestegui@gmail.com>
2023-02-17 10:47:28 -0500

    aqui el mvp3 donde se crea un archivo y se guardan datos en el

--- docker_project_1/mvp3/data.sh
+++ docker_project_1/mvp3/data.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
-
-n1=$(free -h)
-echo "The memory of this computer is:"
-echo ""
-echo "$n1"
+read -p "coloca la cantidad de archivos  " NUM 
+CONT=1
+n=$NUM
+while [ $CONT -le $NUM ]; do
+    sleep 5
+    v1=archivo$CONT.log
+    touch $v1
+    echo "$(free -h)" > $v1
+    CONT=$(($CONT+1))
+done
+echo "se crearon  " $NUM " Archivos"
